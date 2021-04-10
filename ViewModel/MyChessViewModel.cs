e14b3c8661448b3cfe52f4fa6a896a0952d19bc8
Volker Ernst Wollmann <volkerwollmann@t-online.de>
2021-04-10 10:21:38 +0200

    Controls Added

--- ViewModel/MyChessViewModel.cs
+++ ViewModel/MyChessViewModel.cs
@@ -1,20 +1,39 @@
-﻿using System.Windows.Controls;
+﻿using System.Resources;
+using System.Windows;
+using System.Windows.Controls;
 using MyChess.Controls;
 
 namespace MyChess.ViewModel
 {
     public class MyChessViewModel
     {
-        private readonly Grid ChessGrid;
+        private readonly Grid GameGrid;
 
         private readonly ChessMenuUserControl Menu;
 
-        public MyChessViewModel(Grid chessGrid)
+        private readonly ChessBoardUserControl ChessBoard;
+
+        private readonly EngineOutputControl EngineOutput;
+
+        public MyChessViewModel(Grid gameGrid)
         {
-            ChessGrid = chessGrid;
-            
+            GameGrid = gameGrid;
+            RowDefinition r1 = new RowDefinition();
+            GameGrid.RowDefinitions.Add(r1);
+            RowDefinition r2 = new RowDefinition();
+            r2.Height = new GridLength(100);
+            GameGrid.RowDefinitions.Add(r2);
+
             Menu = new ChessMenuUserControl();
-            ChessGrid.Children.Add(Menu);
+            GameGrid.Children.Add(Menu);
+
+            ChessBoard = new ChessBoardUserControl();
+            GameGrid.Children.Add(ChessBoard);
+            Grid.SetRow(ChessBoard, 0);
+
+            EngineOutput = new EngineOutputControl();
+            GameGrid.Children.Add(EngineOutput);
+            Grid.SetRow(EngineOutput, 1);
         }
     }
 }
